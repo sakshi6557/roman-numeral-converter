483af4ebd7610fdac5c2c91be944c95d84c0bcea
sakshi6557 <sakshiureddy@gmail.com>
2025-04-19 12:52:38 -0700

    fixed frontend env issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,48 +1,11 @@
-// import { defineConfig, loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-
-// // https://vite.dev/config/
-// export default defineConfig({
-  
-//   const env = loadEnv(mode, process.cwd(), '')
-//   plugins: [react()],
-//   server: {
-//     port: 8081,
-//   },
-// })
-
-// import { defineConfig, loadEnv } from 'vite'
-// import { fileURLToPath, URL } from 'node:url'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), '')
-
-//   return {
-//     plugins: [react()],
-//     server: {
-//       port: 8081,
-//     },
-//   }
-// })
-
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory
-  const env = loadEnv(mode, process.cwd())
 
-  return {
-    plugins: [react()],
-    define: {
-      // Optional: make custom env variables available globally
-      __APP_ENV__: JSON.stringify(env.VITE_APP_ENV),
-    },
-    server: {
-      port: 8081,
-    },
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8081,
+  },
 })
